98ab0071ae7018960abd782e9ed1f444e1b1e961
Arne Caspari <arne@unicap-imaging.org>
2017-01-09 12:33:10 +0100

    Add python-pyspotify package

---
+++ package/python-pyspotify/python-pyspotify.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# python-pyspotify
+#
+################################################################################
+
+PYTHON_PYSPOTIFY_VERSION = 2.0.5
+PYTHON_PYSPOTIFY_SOURCE = pyspotify-$(PYTHON_PYSPOTIFY_VERSION).tar.gz
+PYTHON_PYSPOTIFY_SITE = https://pypi.python.org/packages/5b/ce/7a6520ea348023762d5b138e85809198626c758ee8053bfc534438ad8bad
+PYTHON_PYSPOTIFY_SETUP_TYPE = setuptools
+PYTHON_PYSPOTIFY_LICENSE = Apache-2.0
+PYTHON_PYSPOTIFY_LICENSE_FILES = LICENSE
+PYTHON_PYSPOTIFY_DEPENDENCIES = host-python-cffi
+
+$(eval $(python-package))

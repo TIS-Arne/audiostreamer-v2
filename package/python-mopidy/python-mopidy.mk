79979e10060f12a60a742fd8fb20a091d9811c80
Arne Caspari <arne@unicap-imaging.org>
2017-01-29 15:44:34 +0100

    Add systemd service files for mopidy

--- package/python-mopidy/python-mopidy.mk
+++ package/python-mopidy/python-mopidy.mk
@@ -12,4 +12,21 @@ PYTHON_MOPIDY_LICENSE = Apache-2.0
 PYTHON_MOPIDY_LICENSE_FILES = LICENSE
 PYTHON_MOPIDY_DEPENDENCIES = python-pykka python-requests python-setuptools python-tornado python-gobject
 
+
+define PYTHON_MOPIDY_USERS
+    mopidy -1 mopidy -1 * - - - Mopidy daemon
+endef
+
+define PYTHON_MOPIDY_INSTALL_INIT_SYSTEMD
+    $(INSTALL) -m 0644 -D $(@D)/extra/systemd/mopidy.service $(TARGET_DIR)/usr/lib/systemd/system/mopidy.service
+    mkdir -p $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants
+    ln -sf /usr/lib/systemd/system/mopidy.service $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/mopidy.service
+endef
+
+define PYTHON_MOPIDY_INSTALL_HOOK
+    $(INSTALL) -m 0755 -D $(@D)/extra/mopidyctl/mopidyctl $(TARGET_DIR)/usr/bin/mopidyctl
+endef
+
+PYTHON_MOPIDY_POST_INSTALL_TARGET_HOOKS	+= PYTHON_MOPIDY_INSTALL_HOOK
+
 $(eval $(python-package))
